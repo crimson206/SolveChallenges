ea081e6aa4d31250f32117e93524ac142c72c5aa
crimson206 <110409356+crimson206@users.noreply.github.com>
2023-01-12 17:27:16 +0000

    test : added the 1st test

--- src/ClassLib/Problems/CodelandUsernameValidation/Solve.cs
+++ src/ClassLib/Problems/CodelandUsernameValidation/Solve.cs
@@ -2,10 +2,24 @@ namespace CodelandUsernameValidation;
 
 public class Solve
 {
+    string strFalse = "false";
+    string strTrue = "true";
+    byte minLetter = (byte)'A';
+    byte maxLetter = (byte)'z';
 
     public string DO(string name)
     {
+        if(CheckLetter(name[0]) == false)
+            return strFalse;        
+
         return "test";
+    }
+
+    public bool CheckLetter(char c)
+    {
+        if(c <= maxLetter && c >= minLetter)
+            return true;
+        return false;
     }
 
 }
