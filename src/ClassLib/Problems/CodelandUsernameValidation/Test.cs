9176c19b73a92da2612042fb6e09d2280eb2ae3d
crimson206 <110409356+crimson206@users.noreply.github.com>
2023-01-12 19:49:25 +0000

    doc : wrote an explanation of the test class

--- src/ClassLib/Problems/CodelandUsernameValidation/Test.cs
+++ src/ClassLib/Problems/CodelandUsernameValidation/Test.cs
@@ -3,14 +3,20 @@ namespace CodelandUsernameValidation;
 
 public class Test
 {
+    /// you can predefine elements that many unittest will use
     string _strFalse = "false";
     string _strTrue = "true";
+
+    /// test target
     private Solver _solver= new Solver();
+
+    /// create a storage of unittest using "delegate"
     public delegate bool DelTest();
     public List<DelTest> allTests = new List<DelTest>();
 
     public Test()
     {
+        /// upload unittest when an instance is created
         allTests.Add(PutFirstCharNotAsLetter_ReturnFalse_1);
         allTests.Add(PutPassableStrings_ReturnTrue_2);
         allTests.Add(PutMiddleSomeCharNotAsLetterNorNumNorUnderscore_ReturnFalse_3);
@@ -18,8 +24,11 @@ public class Test
         allTests.Add(PutNameWihtOutOfRange_ReturnFalse_5);    
     }
 
+    /// a unittest usually has thres parts
+    /// arrange, act and assert 
     public bool PutNameWihtOutOfRange_ReturnFalse_5()
     {
+        /// arrange
         string[] strings =
         {
             "as_",
@@ -29,10 +38,12 @@ public class Test
 
         foreach(var str in strings)
         {
+            /// act
             if(_solver.DO(str) != _strFalse)
+                /// assert
                 return false;
         }
-
+        /// assert
         return true;
     }
 
