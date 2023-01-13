3ff4d4910b172244917a9533c700794390b141a8
crimson206 <110409356+crimson206@users.noreply.github.com>
2023-01-13 22:43:10 +0000

    test : added the 1st unittest
    Testname : InitializeWithWrongArgs_ThrowException_1

--- Tests/Gimmicks/TestImplementationOnSite/CustumTesterTest.cs
+++ Tests/Gimmicks/TestImplementationOnSite/CustumTesterTest.cs
@@ -1,14 +1,73 @@
+using TestImplementationOnSite;
+using System.Collections.Generic;
 namespace Tests;
+
+public static class Class1
+{
+    public static int FromStrIntToInt(string str, int i)
+    {
+        return 1;
+    }
+
+    public static int FromStrsIntsToInt(string[] strs, int[] ints)
+    {
+        return 1;
+    }
+
+    public static int AddTwoNumbers(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+}
+
+public static class TestSets
+{
+    public static object[][] StrIntInputs_L2 = new object[][]
+    {
+        new object[]{"a", 1},
+        new object[]{"b", 2},
+    };
+
+    public static object[][] StrsIntsInputs_L2 = new object[][]
+    {
+        new object[] {new object[]{"a", "b"}, new object[]{1, 2}},
+        new object[] {new object[]{"c", "d"}, new object[]{3, 4}},
+    };
+
+    public static object[] IntResults_L2 = new object[]
+    {
+        1,
+        2
+    };
+
+    public static object[] StrResults_L2 = new object[]
+    {
+        "1",
+        "2"
+    };
+}
 
 [TestClass]
 public class UnitTest1
 {
+
+    [TestMethod]
+    public void InitializeWithWrongArgs_ThrowException_1()
+    {
+
+        Assert.ThrowsException<Exception>(() => new CustomTester(Class1.FromStrIntToInt, TestSets.StrIntInputs_L2, TestSets.StrResults_L2));
+        Assert.ThrowsException<Exception>(() => new CustomTester(Class1.FromStrIntToInt, TestSets.StrsIntsInputs_L2, TestSets.IntResults_L2));
+        
+    }
+
+
     [TestMethod]
     public void PlannedUnittests()
     {
-        /// RegisterMethodWithTestInputsAndExpectedResult_AndReadoutRegisteredMethodsInformation
-        /// DoAllTest_ReturnTestResult_IncludingBoolValueClassNameMethodName
+        /// InitializeWithWrongArgs_ThrowException
+        /// InitializeWithoutName_NameIs_TestMethodName
+        /// InitializeWithName_CheckName
+        /// DoATest_ReturnTrue_IfMethodReturnsExpectedValue
         /// DoATest_ReturnTestResult_IncludingBoolValueClassNameMethodName
-        /// 
     }
 }
--- src/ClassLib/Gimmicks/TestImplementationOnSite/CustumTester.cs
+++ src/ClassLib/Gimmicks/TestImplementationOnSite/CustumTester.cs
@@ -1,7 +1,30 @@
 namespace TestImplementationOnSite;
 
-public class CustumTester
+public class CustomTester
 {
+    string _testName;
+    Delegate _method;
+    object[][] _inputs;
+    object _expectedResults;
 
+    public CustomTester(Delegate method, object[][] inputs, object[] expectedResults)
+    {
+
+        _testName = "Test" + method.Method.Name;
+        _method = method;
+        _inputs = inputs;
+        _expectedResults = expectedResults;
+    }
+
+    public string TestName {get => _testName;}
+
+    public string ReadTests()
+    {
+        string testsInfo = String.Empty;
+
+        testsInfo += "ClassInfo : " + _method!.Method.DeclaringType;
+        testsInfo += ", MethodInfo : " + _method.Method;
+
+        return testsInfo;
+    }
 }
-
