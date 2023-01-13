75868000233b5ef0f525da53de677a8a732e1a8c
crimson206 <110409356+crimson206@users.noreply.github.com>
2023-01-13 15:06:42 +0000

    setup : added CostumTester cls, PlannedTests

---
+++ Tests/Gimmicks/TestImplementationOnSite/CustumTesterTest.cs
@@ -0,0 +1,14 @@
+namespace Tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void PlannedUnittests()
+    {
+        /// RegisterMethodWithTestInputsAndExpectedResult_AndReadoutRegisteredMethodsInformation
+        /// DoAllTest_ReturnTestResult_IncludingBoolValueClassNameMethodName
+        /// DoATest_ReturnTestResult_IncludingBoolValueClassNameMethodName
+        /// 
+    }
+}
---
+++ src/ClassLib/Gimmicks/TestImplementationOnSite/CustumTester.cs
@@ -0,0 +1,7 @@
+namespace TestImplementationOnSite;
+
+public class CustumTester
+{
+
+}
+
